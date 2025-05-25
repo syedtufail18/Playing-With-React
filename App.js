d150d972f5e72743fb3b7f0a678f14add239220f
Your Name <you@example.com>
2025-05-25 21:19:29 +0530

    Comiting changes with new dependancy after installing parcel and running it.

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const element = React.createElement('div', {id:"parent"}, [
-    React.createElement('div', {}, [
-        React.createElement('h1', {id:null}, "Hello World1"),
-        React.createElement('p', {id:null}, "This is a paragraph1")
+const element = React.createElement(
+  'div',
+  { id: 'parent' },
+  [
+    React.createElement('div', { key: 'div1' }, [
+      React.createElement('h1', { key: 'h1-1' }, 'Hello World1'),
+      React.createElement('p', { key: 'p1' }, 'This is a paragraph1')
     ]),
-        
-    React.createElement('div', {},[
-        React.createElement('h1', {id:null}, "Hello World2"),
-        React.createElement('p', {id:null}, "This is a paragraph2")
+    React.createElement('div', { key: 'div2' }, [
+      React.createElement('h1', { key: 'h1-2' }, 'Hello World2'),
+      React.createElement('p', { key: 'p2' }, 'This is a paragraph2')
     ])
-]
-    
+  ]
 );
-//console.log(element);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(element);
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 Hello I'm readme file!!
+
+
+parcel feature(episode 2)
+- Dev build
+- Local Server
+- Caching - Faster build
+- Hot Module Replacement(HMR) using file watching algorithm using c++
+- Live Reloading
+- Build
+- Watch
+- Source Maps
+- Image Optimization
+- CSS Optimization
+- JavaScript Optimization
+- TypeScript Support
+- Consistent Hashing
+- Minification
+- Compress
+- Code splitting
+- Difference Bundling
+- Error Handling
+- HTTPS
+- Tree shaking
+- Differential bundling (support for old browser)
+- Differential dev and prod bundles (npx parcel build index.html) 
+
